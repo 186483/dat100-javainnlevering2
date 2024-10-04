a5ef158e85433d1a41ef51d858f935cda810111c
186483 <alexander.h.log@gmail.com>
2024-10-04 23:04:03 +0200

    Implementert metoder
    Hoppet over den valgfrie 6b oppgaven

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -4,49 +4,102 @@ public class Matriser {
 
 	// a)
 	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+
+		System.out.print("[");
+		for (int i = 0; i < matrise.length; i++) {
+			for (int n = 0; n < matrise[i].length; n++) {
+				System.out.print(matrise[i][n] + " ");
+			}
+
+		}
+		System.out.print("]");
+		System.out.println();
+
 	}
 
 	// b)
 	public static String tilStreng(int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
-		
+		StringBuilder resultat = new StringBuilder();
+
+		for (int i = 0; i < matrise.length; i++) {
+			for (int n = 0; n < matrise[i].length; n++) {
+				resultat.append(matrise[i][n]);
+				if (n < matrise.length) {
+					resultat.append(" ");
+				}
+
+			}
+
+			if (i < matrise[i].length + 1) {
+				resultat.append("\n");
+			}
+		}
+
+		String MatriseStreng = resultat.toString();
+		System.out.println(MatriseStreng);
+		return MatriseStreng;
+
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
-	
+
+		int[][] skaler = new int[matrise.length][];
+		for (int i = 0; i < matrise.length; i++) {
+			skaler[i] = new int[matrise[i].length];
+		}
+
+		for (int i = 0; i < matrise.length; i++) {
+			for (int n = 0; n < matrise[i].length; n++) {
+				skaler[i][n] = tall * matrise[i][n];
+			}
+		}
+		return skaler;
 	}
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
-		
+		boolean bool = true;
+		if (a.length != b.length) {
+			bool = false;
+
+		}
+		if (bool == true) {
+			for (int i = 0; i < a.length; i++) {
+				if (a[i].length != b[i].length) {
+					bool = false;
+					break;
+				}
+
+				if (bool == true) {
+					for (int n = 0; n < a[i].length; n++) {
+						if (a[i][n] != b[i][n]) {
+							bool = false;
+							break;
+						}
+					}
+				}
+			}
+		}
+		return bool;
 	}
-	
+
 	// e)
 	public static int[][] speile(int[][] matrise) {
 
-		// TODO
+		int[][] speil = new int[matrise.length][];
+		for (int i = 0; i < matrise.length; i++) {
+			speil[i] = new int[matrise[i].length];
+		}
 
-		throw new UnsupportedOperationException("Metoden speile ikke implementert");
-	
+		for (int i = 0; i < matrise.length; i++) {
+			for (int n = 0; n < matrise[i].length; n++) {
+				speil[i][n] = matrise [n][i];
+			}
+		}
+		return speil;
 	}
 
-	// f)
-	public static int[][] multipliser(int[][] a, int[][] b) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden multipliser ikke implementert");
-	
-	}
 }
--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -5,59 +5,109 @@ public class Tabeller {
 	// a)
 	public static void skrivUt(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
+		System.out.print("[");
+		for (int i = 0; i < tabell.length; i++) {
+			System.out.print(tabell[i] + " ");
+		}
+		System.out.print("]");
 
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+		StringBuilder resultat = new StringBuilder();
+		resultat.append("[");
+
+		for (int i = 0; i < tabell.length; i++) {
+			resultat.append(tabell[i]);
+
+			if (i < tabell.length - 1) {
+				resultat.append(",");
+			}
+		}
+		resultat.append("]");
+
+		String TabellStreng = resultat.toString();
+		System.out.println(TabellStreng);
+		return TabellStreng;
+
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden summer ikke implementert");
+		int sum = 0;
+		for (int i = 0; i < tabell.length; i++) {
+			sum = sum + tabell[i];
+
+			System.out.println(sum);
+		}
+		return sum;
 	}
 
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden finnesTall ikke implementert");
-
+		boolean bool = false;
+		for (int i = 0; i < tabell.length; i++) {
+			if (tabell[i] == tall) {
+				bool = true;
+				break;
+			}
+		}
+		return bool;
 	}
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden posisjonTall ikke implementert");
+		int posisjon = -1;
+		for (int i = 0; i < tabell.length; i++) {
+			if (tabell[i] == tall) {
+				posisjon = i;
+				break;
+			}
+		}
+		return posisjon;
 	}
 
 	// f)
 	public static int[] reverser(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden reverser ikke implementert");
+		int[] motsatt = new int[tabell.length];
+
+		for (int i = 0; i < tabell.length; i++) {
+			motsatt[i] = tabell[tabell.length - 1 - i];
+		}
+		return motsatt;
 	}
 
 	// g)
 	public static boolean erSortert(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden erSortert ikke implementert");
+		boolean bool = true;
+		for (int i = 1; i < tabell.length; i++) {
+			if (tabell[i] < tabell[i - 1]) {
+				bool = false;
+			}
+		}
+		return bool;
 	}
 
 	// h)
 	public static int[] settSammen(int[] tabell1, int[] tabell2) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden settSammen ikke implementert");
-
+		
+		int TotalLengde = tabell1.length + tabell2.length;
+		int[] Sammen = new int[TotalLengde];
+		
+		for (int i = 0; i < tabell1.length; i++) {
+			Sammen[i] = tabell1[i];
+		}
+		
+		for (int i = 0; i < tabell2.length; i++) {
+			Sammen[tabell1.length + i] = tabell2[i];
+		}
+		return Sammen;
 	}
 }
